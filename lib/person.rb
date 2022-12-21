e799b6f21a3acab17c5fa0277fb1d9952dd158d1
Ben <bboksner@LAPTOP-0U0LFUE0.localdomain>
2022-12-21 04:26:52 -0500

    commit tree 768b4be702cc5bd024b3f828fdf40fe6f8f351fa

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
   # your code here
+  def initialize(attrs)
+    if attrs
+      attrs.each do |key,value|
+        self.class.attr_accessor(key)
+        self.send("#{key}=",value)
+      end
+    end
+    
+  end
 end
